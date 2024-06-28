e45f12a11537b457efc651fea6b5e7b17254b795
mdahmedraza <ahamedraza2233@gmail.com>
2024-06-28 19:30:43 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
+// PS C:\DREAMERS\projects\react-projects\react-food-delivery\food-delivery> git push -u origin master
+// git@github.com: Permission denied (publickey).
+// fatal: Could not read from remote repository.
+
+// Please make sure you have the correct access rights
+// and the repository exists.
